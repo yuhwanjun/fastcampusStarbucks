91d49502072b1cb76ec6540cdf0d067736387f62
Hwanjun <yuhwanjun0311@gmail.com>
2021-03-04 13:57:05 +0000

    22

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        //배지숨기기
+        // gsap.to(요소,시간,옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none';
+        });
+    } else {
+        //배지보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block';
+        });
+    }
+}, 300));
+// _.throttle(함수, 시간)
