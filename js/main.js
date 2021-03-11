1cfc777a1fdc6e3323e3d054850f906d2e11f1cb
Hwanjun <yuhwanjun0311@gmail.com>
2021-03-11 14:27:47 +0000

    ch 10.스타벅스 예제 - 20

--- js/main.js
+++ js/main.js
@@ -35,3 +35,37 @@ window.addEventListener('scroll', _.throttle(function () {
     }
 }, 300));
 // _.throttle(함수, 시간)
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7,
+        opacity: 1
+    });
+});
+
+
+// swiper
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
