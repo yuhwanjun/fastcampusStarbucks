1a6924e30b6212fb1ba667c37ae97fc2670c746f
Hwanjun <yuhwanjun0311@gmail.com>
2021-03-15 14:17:57 +0000

    ch 10. 스타벅스 예제 - 25. 유튜브 영상 배경 - 반복 애니메이션

---
+++ js/youtube.js
@@ -0,0 +1,23 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (a
+function onYouTubeIframeAPIReady() {
+    new YT.Player('player', {
+        videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 videoId
+        playerVars: {
+            autoplay: true,
+            loop: true,
+            playlist: 'An6LvWQuj_8'
+        },
+        events: {
+            onReady: function (event) {
+                event.target.mute() //음소거
+            }
+        }
+    });
+}
